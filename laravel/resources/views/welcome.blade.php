f567a1c94b0761e63c5da93ef3166b625b40d9cd
Sean <44732696+TheSeanU@users.noreply.github.com>
2021-03-05 16:57:52 +0100

    removed

--- laravel/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<?php use partials\header ?>
-  <div class="container-fluid">
-    <div class="row w-100 m-auto">
-      <div class="col m-3">
-        <h4>Groseries</h4>
-        </div>
-      </div>
-  </div>
-<?php use partials\footer ?>
